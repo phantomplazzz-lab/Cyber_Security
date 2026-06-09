6604c3e26b4ede468fce6cc6cdac24f2c0919e78
phantomplazzz-lab <phantomplazzz@gmail.com>
2026-06-09 11:01:10 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Cyber's Love ❤️",
         // Activity type number (0 = Playing).
         type: 0, 
       },
